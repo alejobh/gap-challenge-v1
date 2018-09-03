8167ea35bf547879e125b6ee8e5a0b78f131da7f
Alejandro Bermudez <alejo0395@gmail.com>
2018-09-03 15:48:06 -0500

    new develop branch with changes in the starting app route and using the first example component

---
+++ resources/views/app.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>GAP Challenge</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Poppins', sans-serif;
+                height: 100vh;
+                margin: 0;
+            }
+        </style>
+
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div id="example"></div>
+
+        <script srctype="text/javascript" src="/js/app.js"></script>
+    </body>
+</html>
